5eeba50b92e6953f469895f779a6e03c9a3d9d97
HarutManukyan <harut.manukyan2001@mail.ru>
2022-10-29 21:44:48 +0400

    Harutyun Manukyan

--- src/components/Registration/index.js
+++ src/components/Registration/index.js
@@ -1,19 +1,64 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {LOGIN} from "../../constants";
 
 const Registration = ({navigateTo}) => {
-    const handleRegistration = () => {
-        const userInfoObj = {}
+    const [userName, setUserName] = useState('')
+    const [userEmail, setUserEmail] = useState('')
+    const [userPassword, setUserPassword] = useState('')
 
-        fetch('http://localhost:3000/users', {
-            method: 'POST',
-            body: JSON.stringify(userInfoObj),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-            .then(res => res.json())
-            .then(res => console.log('res', res))
+    const [emptyName, setEmptyName] = useState(false)
+    const [emptyEmail, setEmptyEmail] = useState(false)
+    const [emptyPassword, setEmptyPassword] = useState(false)
+
+    const navigate = useNavigate()
+
+    // creating values--------------------------------
+
+    const signUp = () => {
+
+        if(userName){
+            setEmptyName(false)
+        } else {
+            setEmptyName(true)
+        }   
+
+        if(userEmail){
+            setEmptyEmail(false)
+        } else {
+            setEmptyEmail(true)
+        }
+
+        if(userPassword){
+            setEmptyPassword(false)
+        } else {
+            setEmptyPassword(true)
+
+        const userInfoObj = {
+            id: Math.round(Math.random() * 1000000000),
+            name: userName,
+            email:userEmail,
+            password: userPassword
+        }
+
+            fetch('http://localhost:3000/users', {
+                method: 'POST',
+                body: JSON.stringify(userInfoObj),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then(res => res.json())
+                .then(res => console.log('res', res))
+            
+
+                //navigate('Signin')
+            
+        } 
     }
+
+    // sign up-------------------------------------
+
 
     return (
         <div className='auth-container'>
@@ -21,19 +66,25 @@ const Registration = ({navigateTo}) => {
             <input
                 type="text"
                 className='auth-input'
+                style={emptyName ? {border: '1.5px red solid'}:null}
                 placeholder='username'
+                onChange={(e) => setUserName(e.target.value)}
             />
             <input
                 type="email"
                 className='auth-input'
+                style={emptyEmail ? {border: '1.5px red solid'}:null}
                 placeholder='email'
+                onChange={(e) => setUserEmail(e.target.value)}
             />
             <input
                 type="text"
                 className='auth-input'
+                style={emptyPassword ? {border: '1.5px red solid'}:null}
                 placeholder='password'
+                onChange={(e) => setUserPassword(e.target.value)}
             />
-            <button className='auth-submit'>Register</button>
+            <button onClick={signUp} className='auth-submit'>Register</button>
             <div className='auth-navigate'>
                 <p className='auth-notification'>Already have an account?</p>
                 <button
