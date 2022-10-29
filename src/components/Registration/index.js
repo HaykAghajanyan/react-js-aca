522afc5a986216e4ae6ae0c626dc961cd8707a29
Karen-555 <karenyesayan0@gmail.com>
2022-10-29 16:28:13 +0400

    first merge

--- src/components/Registration/index.js
+++ src/components/Registration/index.js
@@ -1,18 +1,37 @@
+import { useState, useCallback } from "react";
 import {LOGIN} from "../../constants";
+import instance from "../../api/axios";
+
+
 
 const Registration = ({navigateTo}) => {
-    const handleRegistration = () => {
-        const userInfoObj = {}
+    const [userName, setUserName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [isActive, setIsActive] = useState(false)
 
-        fetch('http://localhost:3000/users', {
-            method: 'POST',
-            body: JSON.stringify(userInfoObj),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-            .then(res => res.json())
-            .then(res => console.log('res', res))
+
+    const checkInputFields = useCallback(() => {
+        if(userName === '' || email === ''|| password === '') return true;
+
+    }, [userName, email, password])
+
+
+
+    const handleRegistration = () => {
+        const userInfoObj = {
+            userName,
+            email,
+            password
+        }
+
+        if(!checkInputFields()) {
+            instance.post('users', userInfoObj)
+            .then(navigateTo(LOGIN))
+            .catch(err => console.log('res', err))
+        } else {
+            setIsActive(true)
+        }
     }
 
     return (
@@ -20,20 +39,32 @@ const Registration = ({navigateTo}) => {
             <h3>REGISTRATION</h3>
             <input
                 type="text"
-                className='auth-input'
+                className={isActive  && userName === '' ? 'error-input' : 'auth-input'}
                 placeholder='username'
+                value={userName}
+                onChange={e => setUserName(e.target.value)}
             />
+            {isActive  && userName === '' ? <div className='error-field'>This is required.</div> : <></>}
             <input
                 type="email"
-                className='auth-input'
+                className={isActive  && email === '' ? 'error-input' : 'auth-input'}
                 placeholder='email'
+                value={email}
+                onChange={e => setEmail(e.target.value)}
             />
+            {isActive  && email === '' ? <div className='error-field'>This is required.</div> : <></>}
             <input
                 type="text"
-                className='auth-input'
+                className={isActive  && password === '' ? 'error-input' : 'auth-input'}
                 placeholder='password'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
             />
-            <button className='auth-submit'>Register</button>
+            {isActive  && password === '' ? <div className='error-field'>This is required.</div> : <></>}
+            <button 
+                className='auth-submit'
+                onClick={handleRegistration}
+                >Register</button>
             <div className='auth-navigate'>
                 <p className='auth-notification'>Already have an account?</p>
                 <button
